2b0109c07fab8dab5caa420456b945c63c841c85
ZED <101351154+mastermind-zed@users.noreply.github.com>
2022-06-26 09:35:43 +0000

    .php

---
+++ view-attachment.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Nightingale Jobs - View Attachment</title>
+<link rel="shortcut icon" href="images/ico/favicon.png">
+<link href="css/main.css" rel="stylesheet">
+</head>
+
+<body>
+<?php
+require 'constants/db_config.php';
+$file_id = $_GET['id'];
+
+try {
+$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	
+$stmt = $conn->prepare("SELECT * FROM tbl_other_attachments WHERE id = :fileid");
+$stmt->bindParam(':fileid', $file_id);
+$stmt->execute();
+$result = $stmt->fetchAll();
+
+foreach($result as $row)
+{
+    $certificate = $row['attachment'];
+	$title = $row['title'];
+	
+	?>
+<iframe  style="border:none;" src="ViewerJS/?title=<?php echo "$title"; ?>#<?php echo 'data:application/pdf;base64,'.base64_encode($certificate).'' ?>" height="100%" width="100%"></iframe>
+
+<?php
+}
+
+					  
+}catch(PDOException $e)
+{
+
+}
+
+?>
+</body>
+
+</html>
